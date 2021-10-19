cb49226a2d80656068c0cdfe87dabec18dfeebd4
folka2134 <folka2134@gmail.com>
2021-10-19 18:37:19 +0100

    Homepage now receives and displays data from useGetCryptosQuery

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -9,7 +9,9 @@ const Title = Typography
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery()
-  console.log(data);
+  const globalStats = data?.data?.stats
+  
+  if (isFetching) return "Loading..."
 
   return (
     <>
@@ -17,12 +19,16 @@ const Homepage = () => {
         <h2>Global Crypto Stats</h2>
       </Title>
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Narket Cap" value="5" /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Narket Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={5} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
     </>
   );
 }
